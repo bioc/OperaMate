be75a7736c34745e62feccf69125534ef681fee5
lshep <shepherl@gmail.com>
2017-12-15 09:17:25 -0500

    Deprecate package

--- R/zzz.R
+++ R/zzz.R
@@ -35,3 +35,9 @@
 .onUnload <- function(libname, pkgname) {
     on.exit(options(op), add = TRUE)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.7")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
